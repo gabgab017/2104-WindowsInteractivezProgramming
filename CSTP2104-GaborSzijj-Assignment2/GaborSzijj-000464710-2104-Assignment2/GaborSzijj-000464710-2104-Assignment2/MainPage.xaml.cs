984f4d8261af5fa626575fca8d039a0e79f2cd59
Gabor <gaborszijj01@gmail.com>
2023-10-04 23:59:12 -0700

    Commit1

---
+++ CSTP2104-GaborSzijj-Assignment2/GaborSzijj-000464710-2104-Assignment2/GaborSzijj-000464710-2104-Assignment2/MainPage.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace GaborSzijj_000464710_2104_Assignment2
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void countryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (radioCountry.IsChecked == true)
+            {
+                ComboBoxItem selectedCountryItem = countryComboBox.SelectedItem as ComboBoxItem;
+
+                if (selectedCountryItem != null)
+                {
+                    string countrySelected = selectedCountryItem.Content as string;
+
+                    if (countrySelected != null)
+                    {
+                        switch (countrySelected)
+                        {
+                            case "Canada":
+                                txtCapitalOutput.Text = "Ottawa";
+                                break;
+                            case "Hungary":
+                                txtCapitalOutput.Text = "Budapest";
+                                break;
+                            case "Iran":
+                                txtCapitalOutput.Text = "Tehran";
+                                break;
+                            case "France":
+                                txtCapitalOutput.Text = "Paris";
+                                break;
+                            case "Spain":
+                                txtCapitalOutput.Text = "Madrid";
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void acronymComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (radioAcronym.IsChecked == true)
+            {
+                ComboBoxItem selectedAcronymItem = acronymComboBox.SelectedItem as ComboBoxItem;
+
+                if (selectedAcronymItem != null)
+                {
+                    string acronymSelected = selectedAcronymItem.Content as string;
+
+                    if (acronymSelected != null)
+                    {
+                        switch (acronymSelected)
+                        {
+                            case "CPU":
+                                txtAcronymOutput.Text = "Central processing unit";
+                                break;
+                            case "GPU":
+                                txtAcronymOutput.Text = "Graphics processing unit";
+                                break;
+                            case "HDD":
+                                txtAcronymOutput.Text = "Hard disk drive";
+                                break;
+                            case "SSD":
+                                txtAcronymOutput.Text = "Solid state drive";
+                                break;
+                            case "RAM":
+                                txtAcronymOutput.Text = "Random access memory";
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void radioCountry_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void radioAcronym_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtCapitalOutput_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void txtAcronymOutput_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ CSTP2104-GaborSzijj-Assignment3/CSTP2104-GaborSzijj-Assigment3/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace CSTP2104_GaborSzijj_Assigment3
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnBudapest_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Budapest));
+        }
+
+        private void btnLisbon_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Lisbon));
+        }
+
+        private void btnVienna_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Vienna));
+        }
+
+        private void btnParis_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Paris));
+        }
+
+        private void btnOttawa_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Ottawa));
+        }
+    }
+}
